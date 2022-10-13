5dd8e517371e55b73602f12b4a78180d513b2db5
SuryaPratap2542 <0201cse050@niet.co.in>
2022-10-13 15:55:43 +0530

    main

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 function addUserData(event){
-    event.preventDefault()
+    event.preventDefault();
     var name=document.getElementById('name').value;
     var phone=document.getElementById('phone').value;
     var doctor=document.getElementById('doctor').value;
-    console.log("Name: ",name,phone,doctor);
+
+    var obj = {
+        name: name,
+        phone: phone,
+        doctor: doctor
+    }
+    var key = `user-${phone}`;
+    console.log("Data : ",name,phone,doctor);
+
+    localStorage.setItem(key,JSON.stringify(obj))
+    showOnScreen(obj);
+    document.getElementById('name').value="";
+    document.getElementById('phone').value="";
 }
+function showOnScreen(obj){
+    const parent = document.getElementById('form-ul');
+    const child = document.createElement('li')
+
+    child.innerHTML=` <span id='${obj.phone}'> Name = ${obj.name},Phone = ${obj.phone},Doctor = ${obj.doctor} <button type="delete" onclick="deleteUser( ${obj.phone})">Delete</button> </span>` ;
+
+    parent.append(child);
+}
+document.addEventListener('DOMContentLoaded',show) 
+
+function show(){
+    var user = localStorage.getItem('user')
+    for(var i=0; i<user.length;i++){
+        showOnScreen(user[i]);
+    }
+ }
